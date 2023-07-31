d013eb1e9b4b3e5f025979efec959d40514675c7
Табалов Никита <nikitatabalov@gmail.com>
2023-07-31 20:26:47 +0300

    browserrouter added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Main from './components/Main/Main.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Main />
+		<BrowserRouter>
+			<Main />
+		</BrowserRouter>
 	</React.StrictMode>
 );
